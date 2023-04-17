110af3cd6ba049d8c0e68619c3a98c35d19e45e9
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-04-17 10:39:36 +0900

    計算処理を分割

--- Sample/WindowsFormsApp1/Form1.cs
+++ Sample/WindowsFormsApp1/Form1.cs
@@ -15,9 +15,14 @@ namespace WindowsFormsApp1 {
         }
 
         private void btButton_Click(object sender, EventArgs e) {
-            int sum = int.Parse(tbNum1.Text) + int.Parse(tbNum2.Text);
-            tbAns.Text = sum.ToString() ;
-            
+            //int sum = int.Parse(tbNum1.Text) + int.Parse(tbNum2.Text);
+            //tbAns.Text = sum.ToString() ;
+
+            int num1 = int.Parse(tbNum1.Text);
+            int num2 = int.Parse(tbNum2.Text);
+            int sum = num1 + num2;
+            tbAns.Text = sum.ToString();
+
         }
 
 
