e76e09091e95798e33030744f11df2654f3761af
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-04-19 10:09:28 +0900

    新規作成

---
+++ Sample/BallApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallApp {
+    class Program : Form {
+
+        private Timer moveTimer;    //タイマー用
+        private SoccerBall soccerBall;  //ボール
+        private PictureBox pb;  //画像を表示するコントロール
+
+        static void Main(string[] args) {
+
+            Application.Run(new Program());
+
+        }
+
+        //ctor コンストラクタの自動生成
+        public Program() {
+
+            //this.Width = 1200;  //幅プロパティ
+            //this.Height = 800;  //高さプロパティ
+
+            this.Size = new Size(800, 600); //System.DrawingのSizeクラス
+            this.BackColor = Color.Green;
+            this.Text = "BallGame";
+
+            //ボールインスタンス生成
+            soccerBall = new SoccerBall();
+            pb = new PictureBox();   //画像を表示するコントロール
+            pb.Image = soccerBall.Image;
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+            pb.Size = new Size(50, 50); //画像の表示サイズ
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;  //画像の表示モード
+
+            //画像の登録
+            pb.Parent = this;   //Formを継承している自分(this)を親に設定する
+
+            //タイマーを生成
+            moveTimer = new Timer();
+            moveTimer.Interval = 1; //タイマーのインターバル(ms)
+            moveTimer.Start();  //タイマースタート
+            moveTimer.Tick += MoveTimer_Tick;
+
+        }
+
+        private void MoveTimer_Tick(object sender, EventArgs e) {
+            soccerBall.Move(this,pb);  //移動のメッセージを送る
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+        }
+    }
+}
---
+++ Sample/BallApp/SoccerBall.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallApp {
+    class SoccerBall {
+        //フィールド
+        private Image image;    //画像データ System.DrawingのImageクラス
+
+        private double posX;    //X座標
+        private double posY;    //Y座標
+
+        private double moveX = 5.0;  //移動量(X方向)
+        private double moveY = 5.0;  //移動量(Y方向)
+
+
+
+        //コンストラクタ
+        public SoccerBall() {
+            Image = Image.FromFile(@"pic\soccer_ball.png"); //@を書くと\が文字列として認識される
+            PosX = 0.0;
+            PosY = 0.0;
+        }
+
+        //プロパティ
+        public double PosX { get => posX; set => posX = value; }
+        public double PosY { get => posY; set => posY = value; }
+        public Image Image { get => image; set => image = value; }
+
+        //メソッド
+        public void Move(Form form, PictureBox pb) {
+
+            if (posX > (form.Width - pb.Width) || posX < 0)
+            {
+                moveX = -(moveX);
+            }
+
+            if (posY > (form.Height - pb.Height) || posY < 0)
+            {
+                moveY = -(moveY);
+            }
+            posX += moveX;
+            posY += moveY;
+        }
+
+    }
+}
