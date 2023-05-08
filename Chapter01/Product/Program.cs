259a95182bab06709f06a6d10d2dd1566b920f73
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-05-08 10:54:12 +0900

    P26まで

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProductSample {
+    class Program {
+        static void Main(string[] args) {
+            //インスタンスの生成
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daifuku = new Product(235, "大福もち", 160);
+            Console.WriteLine("税込価格：" + karinto.GetPriceIncludingTax());
+            Console.WriteLine("税込価格：" + daifuku.GetPriceIncludingTax());
+
+        }
+    }
+}
