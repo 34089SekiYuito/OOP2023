956c46bfbeaa7334f6db13a583646a3f8d6f1018
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-05-16 11:47:37 +0900

    varに変更

--- Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
@@ -5,7 +5,7 @@ using System.IO;
 namespace SalesCalculator {
     public class SalesCounter {
 
-        private List<Sale> _sales;
+        private IEnumerable<Sale> _sales;
 
         //コンストラクタ
         public SalesCounter(String filePath) {
@@ -13,9 +13,9 @@ namespace SalesCalculator {
         }
 
         //店舗別売り上げを求める
-        public Dictionary<string, int> GetPerStoreSales() {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
-            foreach (Sale sale in _sales) {
+        public IDictionary<string, int> GetPerStoreSales() {
+            var dict = new SortedDictionary<string, int>();
+            foreach (var sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName)) {
                     //店名がすでに存在する(売り上げ加算)
                     dict[sale.ShopName] += sale.Amount;
@@ -29,15 +29,15 @@ namespace SalesCalculator {
         }
 
         //売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        private List<Sale> ReadSales(String filePath) {
-            List<Sale> sales = new List<Sale>();     //売り上げデータを格納する
-            string[] lines = File.ReadAllLines(filePath);   //ファイルからすべてのデータを読み込む
+        private IEnumerable<Sale> ReadSales(String filePath) {
+            var sales = new List<Sale>();     //売り上げデータを格納する
+            var lines = File.ReadAllLines(filePath);   //ファイルからすべてのデータを読み込む
 
             //すべての行から１行ずつ取り出す
-            foreach (string line in lines) {
-                string[] items = line.Split(',');   //区切りで項目別に分ける
+            foreach (var line in lines) {
+                var items = line.Split(',');   //区切りで項目別に分ける
                 //Saleインスタンスを生成
-                Sale sale = new Sale {
+                var sale = new Sale {
                     ShopName = items[0],
                     ProductCategory = items[1],
                     Amount = int.Parse(items[2])
