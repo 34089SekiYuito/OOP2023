4be8ecb73ca3445b4ea8149dee30504277a53ae1
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-05-17 10:02:33 +0900

    Exercise03 新規作成

---
+++ Chapter02/演習問題/Exercise03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var sales = new SalesCounter(@"data\sales.csv");
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
+                Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+            }
+        }
+    }
+}
