081e4307a75d5f4ad435891955f0faddcd7fab9a
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-05-17 09:38:33 +0900

    演習2

---
+++ Chapter02/演習問題/Eexercise02/InchConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eexercise02 {
+    //インチとメートルの単位変換クラス
+    class InchConverter {
+        //定数
+        private const double ratio = 0.0254;
+
+        //インチからメートルを求める
+        public static double FromMeter(double inch) {
+            return inch * ratio;
+        }
+    }
+}
--- Chapter02/演習問題/Eexercise02/Program.cs
+++ Chapter02/演習問題/Eexercise02/Program.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 namespace Eexercise02 {
     class Program {
         static void Main(string[] args) {
+            PrintToInchMeterList(1, 10);
+        }
+
+        //インチからメートルへの対応表を出力
+        private static void PrintToInchMeterList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.FromMeter(inch);
+                Console.WriteLine("{0} in = {1:0.0000} m", inch, meter);
+            }
         }
     }
 }
