e475240f4c2e8b03d92ccc8dc58d619f5e19b50c
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-05-17 09:21:21 +0900

    演習1

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
+            Song[] songs = new Song[2];
+            songs[0] = new Song("ナンセンス文学", "Eve", 270);
+            songs[1] = new Song("不法侵入", "ずっと真夜中でいいのに", 267);
+
+            foreach (var song in songs) {
+                Console.WriteLine("{0}  {1}  {2}:{3}", song.Title, song.ArtistName, song.Length / 60, song.Length % 60);
+            }
         }
     }
 }
