442cb08f7f34a5cc3df73507b48f5a51100b6fc2
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-05-23 10:30:37 +0900

    P87まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -10,8 +10,11 @@ namespace Section01 {
         static void Main(string[] args) {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
 
-            int count = numbers.Count(n => n % 2 == 0);
-            Console.WriteLine(count);
+            //5の倍数をカウントする
+            //int count = numbers.Count(n => n % 5 == 0 && n > 0);
+
+            int sum = numbers.Where(n => n % 2 == 0).Sum();
+            Console.WriteLine(sum);
         }
     }
 }
