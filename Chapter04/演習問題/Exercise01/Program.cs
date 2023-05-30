41108665b520e8276253f1af4102f89acfd1f91a
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-05-30 11:10:21 +0900

    問題4-1　完成

---
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var ym = new YearMonth(2023, 12);
+            Console.WriteLine(ym.Is21Century);
+            var after = ym.AddOneMonth();
+            Console.WriteLine(after.ToString());
+        }
+    }
+}
---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class YearMonth {
+
+        public int Year { get; private set; }   //年
+        public int Month { get; private set; }  //月
+        public bool Is21Century => (2001 <= Year && Year <= 2100);  //21世紀か
+
+
+        //コンストラクタ
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+
+        //1か月後を求めるメソッド
+        public YearMonth AddOneMonth() {
+            int afterYear = Year;
+            int afterMonth = Month;
+            if (Month == 12) {
+                afterYear++;
+                afterMonth = 1;
+            }
+            else {
+                afterMonth++;
+            }
+            return new YearMonth(afterYear, afterMonth);
+        }
+
+        //ToStringのオーバーライド
+        public override string ToString() {
+            return Year + "年" + Month + "月";
+        }
+    }
+}
