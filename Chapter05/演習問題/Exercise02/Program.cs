109930cbf1ceff8fc0b127fa963d4e70e8823d7d
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-05-31 11:48:44 +0900

    演習問題5-2

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            //5.2
+            Console.Write("数字文字列:");
+            var str = Console.ReadLine();
+
+            int num;
+            int.TryParse(str, out num);
+            Console.WriteLine(num.ToString("#,0"));
+        }
+    }
+}
