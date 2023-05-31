febfb55b5d583070057293ee49e688c76f37f9ce
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-05-31 11:42:25 +0900

    演習問題5-1

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            //5.1
+            Console.Write("文字列1:");
+            var str1 = Console.ReadLine();
+            Console.Write("文字列2:");
+            var str2 = Console.ReadLine();
+
+            if(str1 == str2) 
+                Console.WriteLine("等しい文字列です。");
+            else
+                Console.WriteLine("等しくない文字列です。");
+        }
+    }
+}
