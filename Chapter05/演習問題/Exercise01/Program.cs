6cac8d1f2f123086ffb84a1e31d4cb54a8ef15d0
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-05-31 11:51:45 +0900

    演習問題5-1 修正

--- Chapter05/演習問題/Exercise01/Program.cs
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -15,7 +15,7 @@ namespace Exercise01 {
             Console.Write("文字列2:");
             var str2 = Console.ReadLine();
 
-            if(str1 == str2) 
+            if(String.Compare( str1, str2,ignoreCase:true) == 0) 
                 Console.WriteLine("等しい文字列です。");
             else
                 Console.WriteLine("等しくない文字列です。");
