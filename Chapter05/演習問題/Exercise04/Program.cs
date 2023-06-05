572e6014b065ce43db08bc9e81e274d20184d33e
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-06-05 10:16:07 +0900

    演習問題5-4 完成

---
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "Novellist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var words = text.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Count(); i++) {
+                words[i] = words[i].Substring(words[i].IndexOf('=') + 1);   //'='の次の文字から格納
+            }
+            Console.WriteLine("作家　：{0}", words[0]);
+            Console.WriteLine("代表作：{0}", words[1]);
+            Console.WriteLine("誕生年：{0}", words[2]);
+        }
+    }
+}
