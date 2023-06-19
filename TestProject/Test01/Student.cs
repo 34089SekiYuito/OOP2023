fa493039ffc44a3c3e75896c194d8399230f09ff
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-06-19 15:04:18 +0900

    設問１_Student クラス完成　修正

--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -2,6 +2,6 @@
     class Student {
         public string Name { get; set; }    //学生の名前
         public string Subject { get; set; } //科目名
-        public string Score { get; set; }   //点数
+        public int Score { get; set; }   //点数
     }
 }
