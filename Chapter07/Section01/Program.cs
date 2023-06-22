a583f532d3cff723198606ae1a15da09a2161587
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-06-22 11:12:21 +0900

    県庁所在地　重複処理対応

---
+++ Chapter07/Section01/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+
+            var Prefectures = new Dictionary<string, string>();  //県庁所在地のDictionary
+
+            //入力処理
+            Console.WriteLine("県庁所在地の登録");
+            Console.Write("県名:");   //県名の入力
+            var key = Console.ReadLine();
+
+            while (key != "999") {
+                if (Prefectures.ContainsKey(key))
+                    Console.WriteLine("すでに登録されています。");  //入力された県がすでに存在する場合
+                else {
+                    Console.Write("所在地:");  //市の入力
+                    var value = Console.ReadLine();
+                    Prefectures[key] = value;    //登録
+                }
+                Console.Write("県名:");   //県名の入力
+                key = Console.ReadLine();
+            }
+
+            //出力処理
+            Console.Write("県名を入力:");
+            var select = Console.ReadLine();
+            Console.WriteLine("{0}です。", Prefectures[select]);
+
+            #region  7-1-1
+            //var flowerDict = new Dictionary<string, int>() {
+            //    ["sunflower"] = 400,
+            //    ["pansy"] = 300,
+            //    ["tulip"] = 350,
+            //    ["rose"] = 500,
+            //    ["dahlia"] = 450,
+            //};
+
+            ////morning glory(あさがお)【250円】を追加
+            //flowerDict.Add("morning glory", 250);
+            ////flowerDict["morning glory"] = 250;
+
+            //Console.WriteLine("ひまわりの価格は{0}円です。", flowerDict["sunflower"]);
+            //Console.WriteLine("チューリップの価格は{0}円です。", flowerDict["tulip"]);
+            //Console.WriteLine("あさがおの価格は{0}円です。", flowerDict["morning glory"]);
+            #endregion
+        }
+    }
+}
