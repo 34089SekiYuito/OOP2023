39a3000465df88d95d9bc661ac8dac3dc28bd003
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-06-22 11:24:51 +0900

    県庁所在地　修正　出力時の例外エラー回避

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -9,18 +9,19 @@ namespace Section01 {
         static void Main(string[] args) {
 
             var Prefectures = new Dictionary<string, string>();  //県庁所在地のDictionary
+            string key, value;
 
             //入力処理
             Console.WriteLine("県庁所在地の登録");
             Console.Write("県名:");   //県名の入力
-            var key = Console.ReadLine();
+            key = Console.ReadLine();
 
             while (key != "999") {
                 if (Prefectures.ContainsKey(key))
                     Console.WriteLine("すでに登録されています。");  //入力された県がすでに存在する場合
                 else {
                     Console.Write("所在地:");  //市の入力
-                    var value = Console.ReadLine();
+                    value = Console.ReadLine();
                     Prefectures[key] = value;    //登録
                 }
                 Console.Write("県名:");   //県名の入力
@@ -30,7 +31,10 @@ namespace Section01 {
             //出力処理
             Console.Write("県名を入力:");
             var select = Console.ReadLine();
-            Console.WriteLine("{0}です。", Prefectures[select]);
+            if (Prefectures.ContainsKey(select))
+                Console.WriteLine("{0}です。", Prefectures[select]);
+            else
+                Console.WriteLine("登録されていません。");
 
             #region  7-1-1
             //var flowerDict = new Dictionary<string, int>() {
