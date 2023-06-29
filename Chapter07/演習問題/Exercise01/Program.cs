69e9a18351ff0f9b74a42ee9eb04f7ab89d18bff
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-06-29 11:21:12 +0900

    演習問題7-1 完成

---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            Exercise1_1(text);
+            Console.WriteLine();
+            Exercise1_2(text);
+        }
+
+        private static void Exercise1_1(string text) {
+            var count = new Dictionary<char, int>();
+            foreach (var Char in text.Replace(" ", "").ToUpper()) {
+                if (count.ContainsKey(Char))
+                    count[Char]++;
+                else
+                    count[Char] = 1;
+            }
+            //出力処理
+            foreach (var item in count.OrderBy(c => c.Key)) {
+                Console.WriteLine("\'{0}\':{1}", item.Key, item.Value);
+            }
+        }
+
+        private static void Exercise1_2(string text) {
+            var count = new SortedDictionary<char, int>();
+            foreach (var Char in text.Replace(" ", "").ToUpper()) {
+                if (count.ContainsKey(Char))
+                    count[Char]++;
+                else
+                    count[Char] = 1;
+            }
+            //出力処理
+            foreach (var item in count) {
+                Console.WriteLine("\'{0}\':{1}", item.Key, item.Value);
+            }
+        }
+    }
+}
