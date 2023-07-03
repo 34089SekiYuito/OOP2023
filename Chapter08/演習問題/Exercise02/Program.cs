101ebf7244825bbeccc915b6a50e0fbc6b8134e0
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-07-03 13:39:38 +0900

    演習問題8-2 完成

---
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+            DateTime nextday;
+            var culture = new CultureInfo("ja-JP");
+
+            foreach (DayOfWeek dayOfWeek in Enum.GetValues(typeof(DayOfWeek))) {
+                nextday = NextDay(dateTime, dayOfWeek);
+                culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+                Console.WriteLine("{0}の次週の{1}は{2}", dateTime.ToString("yyyy/MM/dd"), dayOfWeek, nextday.ToString("yyyy/M/dd(ddd)"));
+            }
+        }
+
+        private static DateTime NextDay(DateTime dateTime, DayOfWeek dayOfWeek) {
+            var days = (int)dayOfWeek - (int)dateTime.DayOfWeek;
+            days += 7;
+            return dateTime.AddDays(days);
+        }
+    }
+}
