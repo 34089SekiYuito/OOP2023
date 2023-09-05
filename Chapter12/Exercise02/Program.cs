90f01e79ceeb0c170e21dfd6bca461425ab2a18e
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-09-05 13:15:22 +0900

    練習問題12-2　途中

--- Chapter12/Exercise02/Program.cs
+++ Chapter12/Exercise02/Program.cs
@@ -2,34 +2,46 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
             var novelist = Exercise2_1("sample.xml");
+            Console.WriteLine(novelist);
             Exercise2_2(novelist, "novelist.json");
 
-            //// これは確認のためのコード 12.2.1
-            //Console.WriteLine("{0} {1}", novelist.Name, novelist.Birth);
-            //foreach (var title in novelist.Masterpieces) {
-            //    Console.WriteLine(title);
-            //}
-            //Console.WriteLine();
+            // これは確認のためのコード 12.2.1
+            Console.WriteLine("{0} {1}", novelist.Name, novelist.Birth);
+            foreach (var title in novelist.Masterpieces) {
+                Console.WriteLine(title);
+            }
+            Console.WriteLine();
 
-            //// これは確認のためのコード 12.2.2
-            //Console.WriteLine(File.ReadAllText("novelist.json"));
-            //Console.WriteLine();
+            // これは確認のためのコード 12.2.2
+            Console.WriteLine(File.ReadAllText("novelist.json"));
+            Console.WriteLine();
         }
 
-        private static object Exercise2_1(string v) {
-
-
+        private static Novelist Exercise2_1(string v) {
+            using (var reder = XmlReader.Create(v)) {
+                var serializer = new XmlSerializer(typeof(Novelist));
+                var novelist = (Novelist)serializer.Deserialize(reder);
+                return novelist;
+            }
         }
 
         private static void Exercise2_2(object novelist, string v) {
-
+            using (var stream = new FileStream(v, FileMode.Create, FileAccess.Write)) {
+                var seralizer = new DataContractJsonSerializer(novelist.GetType());
+                seralizer.WriteObject(stream, novelist);
+            }
         }
     }
+
+
 }
