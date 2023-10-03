059dbb45e021195332243f46479c536c60c4e6c7
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-10-03 09:33:45 +0900

    RSSひな形完成

--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -4,14 +4,37 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace RssReader {
     public partial class Form1 : Form {
+        List<itemData> nodes;
         public Form1() {
             InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbUrl.Text);
+                XDocument xdoc = XDocument.Load(url);
+
+                nodes = xdoc.Root.Descendants("item").Select(x => new itemData {
+                                                                Title = x.Element("title").Value ,
+                                                                Link = x.Element("link").Value
+                                                                }).ToList();
+                lbRssTitle.Items.Clear();
+                foreach (var node in nodes) {
+                    lbRssTitle.Items.Add(node.Title);
+                }
+            }
+        }
+
+        private void lbRssTitle_Click(object sender, EventArgs e) {
+            wbBrowser.Url =  new Uri(nodes[lbRssTitle.SelectedIndex].Link);
         }
     }
 }
