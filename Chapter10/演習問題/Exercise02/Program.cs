4216c948c2b71bf79b3e00297b2002d554f5a9b0
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-10-05 09:35:18 +0900

    演習問題10-2

---
+++ Chapter10/演習問題/Exercise02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Picup3DigitNumber("sample.txt");
+        }
+
+        private static void Picup3DigitNumber(string file) {
+            foreach (var word in File.ReadAllText(file).Split(' ')) {
+                if (Regex.IsMatch(word, @"^\d{3,}$"))
+                    Console.WriteLine(word);
+            }
+        }
+    }
+}
