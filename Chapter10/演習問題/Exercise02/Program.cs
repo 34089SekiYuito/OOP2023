3f4a2a7a84d850037bbd5369fd6487e0de1d6d0d
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-10-05 10:52:59 +0900

    演習問題10-2 修正

--- Chapter10/演習問題/Exercise02/Program.cs
+++ Chapter10/演習問題/Exercise02/Program.cs
@@ -13,9 +13,12 @@ namespace Exercise02 {
         }
 
         private static void Picup3DigitNumber(string file) {
-            foreach (var word in File.ReadAllText(file).Split(' ')) {
-                if (Regex.IsMatch(word, @"^\d{3,}$"))
-                    Console.WriteLine(word);
+            foreach (var word in File.ReadLines(file)) {
+                var matches = Regex.Matches(word, @"\b\d{3,}\b");
+                foreach (Match match in matches) {
+                    if (match.Success)
+                        Console.WriteLine(match.Value);
+                }
             }
         }
     }
