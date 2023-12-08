ec2fb8a6db1ee7f420ae695d762fb1f2266b396e
関　唯斗 <OIBC34089@ojs.ac.jp>
2023-12-08 11:47:22 +0900

    APIで情報を取得

--- API/API/Form1.cs
+++ API/API/Form1.cs
@@ -7,33 +7,41 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 //楽天　パスワード　OIBC34089@
 
-namespace API {
-    public partial class Form1 : Form {
+namespace API
+{
+    public partial class Form1 : Form
+    {
         List<itemData> nodes;
         List<string> topics = new List<string>();
-        string baseUrl = @"https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?format=xml&hotelNo=123456&applicationId=e06e2a5afcf14b52139c1fb6c58e9dbc";
-        private HttpClient httpClient;
+        //SampleServiceHttpClient test;
 
-        public Form1() {
+        public Form1()
+        {
             InitializeComponent();
             topics = GetList("RssUrl.txt");
-            httpClient = new HttpClient();
+            //test = new SampleServiceHttpClient(@"https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?format=xml&applicationId=1079385179510929202");
         }
 
         //RssのUrlを取得
-        private List<string> GetList(string Path) {
+        private List<string> GetList(string Path)
+        {
             List<string> Topics = new List<string>();
-            if (File.Exists(Path)) {
-                using (var reader = new StreamReader(Path, Encoding.UTF8)) {
-                    while (!reader.EndOfStream) {
+            if (File.Exists(Path))
+            {
+                using (var reader = new StreamReader(Path, Encoding.UTF8))
+                {
+                    while (!reader.EndOfStream)
+                    {
                         var line = reader.ReadLine();
                         Topics.Add(line);
                     }
@@ -43,10 +51,12 @@ namespace API {
         }
 
         //取得ボタンクリック
-        private void btGet_Click(object sender, EventArgs e) {
-            
-            var list =XDocument.Load("https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?format=xml&hotelNo=123456&applicationId=e06e2a5afcf14b52139c1fb6c58e9dbc&hotelNo=123456");
-            var names = list.Descendants();
+        private void btGet_Click(object sender, EventArgs e)
+        {
+
+
+
+            //var str = test.GetSample("a");
 
             //lbRssTitle.Items.Clear();
             //nodes = null;
@@ -64,11 +74,13 @@ namespace API {
         }
 
         //登録ボタンクリック
-        private void btRegister_Click(object sender, EventArgs e) {
+        private void btRegister_Click(object sender, EventArgs e)
+        {
             if (tbLink.Text == "") return;
             if (tbRegister.Text == "") return;
 
-            var data = new itemData {
+            var data = new itemData
+            {
                 Title = tbRegister.Text,
                 Link = tbLink.Text,
             };
@@ -80,39 +92,122 @@ namespace API {
         }
 
         //ListBoxが選択された時
-        private void lbRssTitle_Click(object sender, EventArgs e) {
+        private void lbRssTitle_Click(object sender, EventArgs e)
+        {
             if (lbRssTitle.SelectedIndex == -1) return;
             wbBrowser.Navigate(nodes[lbRssTitle.SelectedIndex].Link);
         }
 
         //RadioButtonが選択された時
-        private void rb_CheckedChanged(object sender, EventArgs e) {
-            if (topics.Count != 0) {
-                foreach (RadioButton item in gbTopics.Controls) {
+        private void rb_CheckedChanged(object sender, EventArgs e)
+        {
+            if (topics.Count != 0)
+            {
+                foreach (RadioButton item in gbTopics.Controls)
+                {
                     if (item.Checked)
                         tbLink.Text = topics[int.Parse(item.Tag.ToString())];
                 }
             }
-            else {
+            else
+            {
                 MessageBox.Show("URLが登録されていません");
             }
             cbUrl.SelectedIndex = -1;
         }
 
         //登録されたItemが選択された時
-        private void cbUrl_SelectionChangeCommitted(object sender, EventArgs e) {
+        private void cbUrl_SelectionChangeCommitted(object sender, EventArgs e)
+        {
             var item = (itemData)((ComboBox)sender).SelectedItem;
             if (item == null) return;
             tbLink.Text = item.Link;
         }
 
         //登録する名前とURLが重複しているか調べる
-        private bool itemsContains(itemData data) {
-            foreach (itemData item in cbUrl.Items) {
+        private bool itemsContains(itemData data)
+        {
+            foreach (itemData item in cbUrl.Items)
+            {
                 if (item.Title.Contains(data.Title) || item.Link.Contains(data.Link))
                     return false;
             }
             return true;
         }
     }
+
+    #region API
+    //internal class SampleServiceHttpClient
+    //{
+    //    /// <summary>
+    //    /// 通信先のベースURL
+    //    /// </summary>
+    //    public string baseUrl;
+    //    /// <summary>
+    //    /// C#側のHttpクライアント
+    //    /// </summary>
+    //    public HttpClient httpClient;
+
+    //    /// <summary>
+    //    /// デフォルトコンストラクタ。外部からは呼び出せません。
+    //    /// </summary>
+    //    private SampleServiceHttpClient()
+    //    {
+    //    }
+
+    //    /// <summary>
+    //    /// 引数付きのコンストラクタ。こちらを使用します。
+    //    /// 引数には正しいURLが入っていることが前提です。
+    //    /// </summary>
+    //    /// <param name="baseUrl">ベースのURL</param>
+    //    public SampleServiceHttpClient(string baseUrl)
+    //    {
+    //        this.baseUrl = baseUrl;
+    //        // 通信するメソッドでその都度HttpClientをnewすると毎回ソケットを
+    //        // 開いてリソースを消費するため、
+    //        // メンバ変数で使い回す手法を取っています。
+    //        this.httpClient = new HttpClient();
+    //    }
+
+    //    public string GetSample(string someId)
+    //    {
+    //        String requestEndPoint = this.baseUrl + "/some/search/?someId=" + "&largeClassCode=japan&middleClassCode=nagasaki&smallClassCode=nagasaki";
+    //        HttpRequestMessage request
+    //            = new HttpRequestMessage(HttpMethod.Get, requestEndPoint);
+
+    //        string resBodyStr;
+    //        HttpStatusCode resStatusCoode = HttpStatusCode.NotFound;
+    //        Task<HttpResponseMessage> response;
+    //        // 通信実行。メンバ変数でhttpClientを持っているので、using(～)で囲いません。
+    //        // 囲うと通信後にオブジェクトが破棄されます。
+    //        // 引数にrequestを取る場合はGetAsyncやPostAsyncでなくSendAsyncメソッドになります。
+    //        // 戻り値はTask<HttpResponseMessage>で、変数名.Resultとすると
+    //        // System.Net.Http.HttpResponseMessageクラスが取れます。
+    //        try
+    //        {
+    //            response = httpClient.SendAsync(request);
+    //            resBodyStr = response.Result.Content.ReadAsStringAsync().Result;
+    //            resStatusCoode = response.Result.StatusCode;
+    //        }
+    //        catch (HttpRequestException e)
+    //        {
+    //            // UNDONE: 通信失敗のエラー処理
+    //            return null;
+    //        }
+
+    //        if (!resStatusCoode.Equals(HttpStatusCode.OK))
+    //        {
+    //            // UNDONE: レスポンスが200 OK以外の場合のエラー処理
+    //            return null;
+    //        }
+    //        if (String.IsNullOrEmpty(resBodyStr))
+    //        {
+    //            // UNDONE: レスポンスのボディが空の場合のエラー処理
+    //            return null;
+    //        }
+    //        // 中身のチェックなどを経て終了。
+    //        return resBodyStr;
+    //    }
+    //}
+    #endregion
 }
---
+++ API/APItest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace APItest
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String url = "https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?format=json&applicationId=1079385179510929202" + "&largeClassCode=japan&middleClassCode=nagasaki&smallClassCode=nagasaki";
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+
+            Stream stream = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream, Encoding.UTF8);
+            string text = streamReader.ReadToEnd();
+            streamReader.Close();
+            stream.Close();
+            Console.WriteLine(text);
+
+            //Stream response_stream = request.GetResponse().GetResponseStream();
+            //StreamReader reader = new StreamReader(response_stream);
+            //var obj_from_json = JObject.Parse(reader.ReadToEnd());
+
+            //var forecast_sum = obj_from_json["weather"][0]["main"];
+            //var forecast_des = obj_from_json["weather"][0]["description"];
+            //var forecast_max_temp = obj_from_json["main"]["temp_max"];
+            //var forecast_min_temp = obj_from_json["main"]["temp_min"];
+            //var forecast_hum = obj_from_json["main"]["humidity"];
+            //var forecast_wind = obj_from_json["wind"]["speed"];
+
+            //string forecast_output = "Tokyo is " + forecast_sum + " now. Description is " + forecast_des + ". Max tempreture is " + forecast_max_temp
+            //    + "℃. Min tempreture is " + forecast_min_temp + "℃. Humidity is " + forecast_hum + "%. Wind is " + forecast_wind + "m/s. Have a good day!";
+            //Console.WriteLine(forecast_output);
+
+        }
+    }
+}
